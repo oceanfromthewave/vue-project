619d5fb50ce7cb7d35146d3458b7d5e0104928a1
oceanfromthewave <oceanfromthewave@gmail.com>
2022-11-07 17:20:26 +0900

    Initial commit

--- src/pages/PostCode.vue
+++ src/pages/PostCode.vue
@@ -1,6 +1,6 @@
 <template>
   <form>
-  <input type="text" :value="postcode" placeholder="우편번호" />
+  <input type="text" :value="data.zonecode" placeholder="우편번호" />
   <input type="button" @click="handleClick()" value="우편번호 찾기" />
   <input type="text" :value="address" placeholder="주소" />
   <input type="text" ref="detail" placeholder="상세주소" />
@@ -17,9 +17,23 @@ form{
 export default {
   data() {
     return {
+      data:{},
       address: "",
       extraAddr: "",
-      postcode: "",
+    
+    }
+  },
+    computed:{
+      address(){
+    //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+        if (this.data.userSelectedType === "R") {
+    // 사용자가 도로명 주소를 선택했을 경우
+    return this.data.roadAddress;
+  } else {
+    // 사용자가 지번 주소를 선택했을 경우(J)
+    return this.data.jibunAddress;
+  
+       }
     }
   },
   methods: {
@@ -31,22 +45,12 @@ export default {
   },
 };
 function oncomplete(data) {
+  this.data = data;
   // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
   // 각 주소의 노출 규칙에 따라 주소를 조합한다.
   // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-  var addr = ""; // 주소 변수
-  var extraAddr = ""; // 참고항목 변수
-
-  //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-  if (data.userSelectedType === "R") {
-    // 사용자가 도로명 주소를 선택했을 경우
-    addr = data.roadAddress;
-  } else {
-    // 사용자가 지번 주소를 선택했을 경우(J)
-    addr = data.jibunAddress;
-  }
-
+  var extraAddr = ''; // 참고항목 변수
   // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
   if (data.userSelectedType === "R") {
     // 법정동명이 있을 경우 추가한다. (법정리는 제외)
@@ -66,12 +70,12 @@ function oncomplete(data) {
     // 조합된 참고항목을 해당 필드에 넣는다.
     this.extraAddr = extraAddr;
   } else {
-    this.extraAddr = "";
+    this.extraAddr = '';
   }
 
   // 우편번호와 주소 정보를 해당 필드에 넣는다.
-  this.postcode = data.zonecode;
-  this.address = addr;
+  // this.postcode = data.zonecode;
+
   // 커서를 상세주소 필드로 이동한다.
   this.$refs.detail.focus();
 }
